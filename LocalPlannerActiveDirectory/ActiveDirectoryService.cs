2be2e3f6d88afabcab7994d4e3199fa84375bf39
thomaskleist <thomas@kleist-it.com>
2020-03-23 21:06:09 +0100

    * .gitignore: Create Service for getting group users
    * LocalPlannerActiveDirectory.sln:
    * ActiveDirectoryService.cs:
    * LocalPlannerActiveDirectory.csproj:

---
+++ LocalPlannerActiveDirectory/ActiveDirectoryService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+namespace LocalPlannerActiveDirectory
+{
+    public class ActiveDirectoryService
+    {
+        public List<GroupPrincipal> GetGroupsForUser(string username)
+        {
+            try
+            {
+                List<GroupPrincipal> result = new List<GroupPrincipal>();
+                PrincipalContext principalContext = new PrincipalContext(ContextType.Domain);
+                var userprincipal = UserPrincipal.FindByIdentity(principalContext, username);
+                PrincipalSearchResult<Principal> authGroups = userprincipal.GetAuthorizationGroups();
+                
+                foreach (Principal o in authGroups)
+                {
+                    result.Add((GroupPrincipal)o);
+                }
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+          
+        }
+    }
+}
